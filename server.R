41f027e00f244dd6e49ee61a46da0bdb295b1934
Alberto Sanchez rodelgo <alberto.sanchez.rodelgo@gmail.com>
2017-11-14 15:13:43 -0500

    Primera ui/server interaccion

--- server.R
+++ server.R
@@ -1,1 +1,13 @@
 ### Server General
+
+shinyServer(function(input, output, session) {
+  
+  source("server/server_Presupuesto.R", local=TRUE)$value
+  
+  output$Noticias <- renderPlot({
+    
+    plot(mtcars$mpg,mtcars$cyl)
+    
+  })
+  
+})
--- ui.R
+++ ui.R
@@ -4,45 +4,45 @@ tags$div(height="70%")
 navbarPage(tags$img(src = "logo.png", width = "100%", height = "90px"),
            navbarMenu("Inicio",
                   tabPanel("¿Que es el OGP?",
-                           tags$h3("¿Que es el OGP?")  
+                           h3("¿Que es el OGP?")  
                   
                     ),
                   tabPanel("Marco Jurídico",
-                           tags$h3("Marco Jurídico")  
+                           h3("Marco Jurídico")  
                            
                   ),
                   tabPanel("¿Cómo funciona?",
-                           tags$h3("¿Cómo funciona?")  
+                           h3("¿Cómo funciona?")  
                            
                   )),
            navbarMenu("Ejes",
                     tabPanel("Salud",
-                      tags$h3("Salud")
+                      h3("Salud")
                     ),
                     tabPanel("Educacion",
-                      tags$h3("Educacion")
+                      h3("Educacion")
                     ),
                     tabPanel("Seguridad",
-                      tags$h3("Seguridad")
+                      h3("Seguridad")
                     ),
                     tabPanel("Infraestructura",
-                      tags$h3("Infraestructura")
+                      h3("Infraestructura")
                   )),
            navbarMenu("Infórmate",
                       tabPanel("Noticias",
-                               verbatimTextOutput("Noticias")
+                               plotOutput("Noticias")
                       ),
                       tabPanel("Presupuesto nacional",
-                               verbatimTextOutput("Noticias")
+                               source("ui/ui_Presupuesto.R", local = TRUE)$value
                       ),
                       tabPanel("Videoteca",
-                               tags$h3("Videoteca")
+                               h3("Videoteca")
                       ),
                       tabPanel("Centro de documentación",
-                               tags$h3("Centro de Documentación")
+                               h3("Centro de Documentación")
                       )),
            tabPanel("Enlaces",
-                                tags$h3("Enlaces")
+                                h3("Enlaces")
                        )
                       
            )
