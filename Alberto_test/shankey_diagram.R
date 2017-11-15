416159af75b5a6d7dd3c40b2fd581b8a5ff56708
Alberto Sanchez rodelgo <alberto.sanchez.rodelgo@gmail.com>
2017-11-15 16:48:40 -0500

    sankey chart funcionando. Quitado los factores con monto = 0

--- Alberto_test/shankey_diagram.R
+++ Alberto_test/shankey_diagram.R
@@ -10,12 +10,20 @@ datos_origen <- group_by(datos_origen, Grupo.Gasto,Fuente.Financiamiento) %>%
   filter(monto > 0) %>% 
   as.data.frame()
 
+filtered_nodes <- c(unique(as.character(datos_origen$Grupo.Gasto)),unique(as.character(datos_origen$Fuente.Financiamiento)))
+
 nodes <- data.frame(name = c(levels(datos_origen$Grupo.Gasto),levels(datos_origen$Fuente.Financiamiento))) %>%
   mutate_all(as.character) %>%
+  filter(name %in% filtered_nodes) %>%
   as.data.frame()
 
+datos_origen <- mutate_if(datos_origen, is.factor,as.character) %>%
+  as.data.frame()
+
+datos_origen$Grupo.Gasto <- as.factor(datos_origen$Grupo.Gasto)
 levels(datos_origen$Grupo.Gasto) <- c(0:(length(levels(datos_origen$Grupo.Gasto))-1))
-levels(datos_origen$Fuente.Financiamiento) <- c(length(levels(datos_origen$Grupo.Gasto)):(length(levels(datos_origen$Fuente.Financiamiento))+length(levels(datos$Grupo.Gasto))))
+datos_origen$Fuente.Financiamiento <- as.factor(as.character(datos_origen$Fuente.Financiamiento))
+levels(datos_origen$Fuente.Financiamiento) <- c(length(levels(datos_origen$Grupo.Gasto)):(length(levels(datos_origen$Fuente.Financiamiento))+length(levels(datos_origen$Grupo.Gasto))))
 
 datos <- select(datos_origen, source = Fuente.Financiamiento, target = Grupo.Gasto, value = monto) %>%
   mutate_all(as.character) %>%
@@ -26,14 +34,14 @@ sankeyNetwork(Links = datos, Nodes = nodes, Source = "source",
               Target = "target", Value = "value", NodeID = "name",
               units = "", fontSize = 12, nodeWidth = 30)
 
-
-# # Load energy projection data
-URL <- paste0(
-  "https://cdn.rawgit.com/christophergandrud/networkD3/",
-  "master/JSONdata/energy.json")
-Energy <- jsonlite::fromJSON(URL)
-# Plot
-sankeyNetwork(Links = Energy$links, Nodes = Energy$nodes, Source = "source",
-              Target = "target", Value = "value", NodeID = "name",
-              units = "TWh", fontSize = 12, nodeWidth = 30)
+# 
+# # # Load energy projection data
+# URL <- paste0(
+#   "https://cdn.rawgit.com/christophergandrud/networkD3/",
+#   "master/JSONdata/energy.json")
+# Energy <- jsonlite::fromJSON(URL)
+# # Plot
+# sankeyNetwork(Links = Energy$links, Nodes = Energy$nodes, Source = "source",
+#               Target = "target", Value = "value", NodeID = "name",
+#               units = "TWh", fontSize = 12, nodeWidth = 30)
 
