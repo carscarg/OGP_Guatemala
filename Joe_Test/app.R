74fe4e689171ae8ca6f65ce9cdf9d9714cec4e58
Luz Angelina Rodríguez de Cole <jcolev@ufm.edu>
2017-11-16 15:53:26 -0600

    cagadal

--- Joe_Test/app.R
+++ Joe_Test/app.R
@@ -1,20 +1,45 @@
 library(rpivotTable)
 library(shiny)
 library(dplyr)
-
+library(highcharter)
+library(reshape2)
 
 tabla_cruda <- readRDS("formulacion2018.rds")
 df <- group_by(tabla_cruda,Entidad,Programa,Unidad.Ejecutora,Grupo.Gasto,Tipo.de.Gasto,Fuente.Financiamiento) %>% summarise(monto = sum(Recomendado.2018, na.rm=TRUE))
+#datos <- group_by(df,Entidad) %>% summarise(monto = sum(monto))
 
 server <- function(input, output) {
-
-
+  
+    output$tabla <- renderDataTable({
+    #cadena <- paste0(input$myPivotData$rows[[1]],input$myPivotData$cols[[1]],sep =",")
+    
+    datos <- group_by_(df,input$myPivotData$rows[[1]],input$myPivotData$cols[[1]]) %>% summarise(monto = sum(monto))
+    datos <-spread_(data = datos,key = input$myPivotData$cols[[1]],value = "monto")
+    
+    datatable(datos, extensions="Scroller", style="bootstrap", class="compact", width="100%",
+              options=list(dom="ft",deferRender=TRUE, scroller=FALSE)) %>%
+      formatCurrency(c(2)) %>% 
+      formatStyle(c(0), target='row', fontSize = '80%')
+    
+  })
+  output$barras <- renderHighchart({
+    datos <- group_by_(df,input$myPivotData$rows[[1]]) %>% summarise(monto = sum(monto))
+    
+    highchart() %>% 
+      hc_chart(type = "column") %>% 
+      hc_title(text = "bla") %>% 
+      hc_xAxis(categories = datos$Entidad) %>% 
+      hc_add_series(data = datos$monto,
+                    colorByPoint = TRUE,
+                    name = "Monto")
+    
+  })
   
   output$mypivot = renderRpivotTable({
-    rpivotTable(data=df, onRefresh=htmlwidgets::JS("function(config) { Shiny.onInputChange('myPivotData', config); }"))
+    rpivotTable(data=df, rows="Entidad",col ="Tipo.de.Gasto",onRefresh=htmlwidgets::JS("function(config) { Shiny.onInputChange('myPivotData', config); }"))
   
     })
-  output$quehay <- renderTable(as.tibble(str(input$myPivotData$cols)))
+  output$quehay <- renderTable(as.tibble(str(input$myPivotData$rows)))
 }
 
 ui <- shinyUI(fluidPage(
@@ -24,8 +49,13 @@ ui <- shinyUI(fluidPage(
   ),
   fluidRow(
   column(12,tableOutput("quehay"))
-  
-)
+  ),
+  fluidRow(
+    column(12,highchartOutput("barras"))
+    ),
+  fluidRow(
+    column(12,dataTableOutput("tabla"))
+  )
 ))
 
 shinyApp(ui = ui, server = server) 
