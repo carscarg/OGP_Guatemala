9fb029a7b1134cc91a56821d329f42d05577f569
Luz Angelina Rodríguez de Cole <jcolev@ufm.edu>
2017-11-16 09:28:10 -0600

    creando test Joe

---
+++ Joe_Test/app.R
@@ -0,0 +1,47 @@
+library(rpivotTable)
+library(shiny)
+
+
+
+
+list_to_string <- function(obj, listname) {
+  if (is.null(names(obj))) {
+    paste(listname, "[[", seq_along(obj), "]] = ", obj,
+          sep = "", collapse = "\n")
+  } else {
+    paste(listname, "$", names(obj), " = ", obj,
+          sep = "", collapse = "\n")
+  }
+}
+
+server <- function(input, output) {
+  
+  output$pivotRefresh <- renderText({
+    
+    cnames <- list("cols","rows","vals", "exclusions","aggregatorName", "rendererName")
+    # Apply a function to all keys, to get corresponding values
+    allvalues <- lapply(cnames, function(name) {
+      item <- input$myPivotData[[name]]
+      if (is.list(item)) {
+        list_to_string(item, name)
+      } else {
+        paste(name, item, sep=" = ")
+      }
+    })
+    paste(allvalues, collapse = "\n")
+  })
+  
+  output$mypivot = renderRpivotTable({
+    rpivotTable(data=cars, onRefresh=htmlwidgets::JS("function(config) { Shiny.onInputChange('myPivotData', config); }"))
+  })
+}
+
+ui <- shinyUI(fluidPage(
+ # HTML('<script type="text/javascript" src="https://www.google.com/jsapi"></script>'),
+  #htmlwidgets::JS('google.load("visualization", "1", {packages:["corechart", "charteditor"]});'),
+  fluidRow(
+    column(6, rpivotTableOutput("mypivot") ))
+)
+)
+
+shinyApp(ui = ui, server = server) 
